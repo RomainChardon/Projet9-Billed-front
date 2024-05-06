0764e5a20975e0274c3fd13a07b60ef6502f51b0
RomainChardon <chardon.romain26@gmail.com>
2024-05-06 09:19:15 +0200

    disable event

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -86,6 +86,10 @@ export default class {
   }
 
   handleEditTicket(e, bill, bills) {
+    console.log(e)
+    console.log(this.id)
+    console.log(bill)
+    console.log(bills)
     if (this.counter === undefined || this.id !== bill.id) this.counter = 0
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
     if (this.counter % 2 === 0) {
@@ -130,6 +134,7 @@ export default class {
     this.onNavigate(ROUTES_PATH['Dashboard'])
   }
 
+  // affiche les tickets par catégorie
   handleShowTickets(e, bills, index) {
     if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.index === undefined || this.index !== index) this.index = index
@@ -145,7 +150,10 @@ export default class {
       this.counter ++
     }
 
+    // Click pour ouvrir
     bills.forEach(bill => {
+      // Désactiver l'event avant d'en ouvrir un autre
+      $(`#open-bill${bill.id}`).off("click");
       $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
     })
 
